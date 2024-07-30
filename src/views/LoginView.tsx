f4293e689184054fac4a978365fb6eebaf4ed255
Miguel Cobian <miguelcobian99@gmail.com>
2024-07-30 14:54:45 -0600

    Style. Center forms

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -40,55 +40,54 @@ const LoginView = () => {
     }
 
     return (
-        <div className="container w-50">
-            <h1 className="text-center">Task4</h1>
-            <hr />
-            <h2 className="mt-10">Sign In</h2>
-            {errorAPI && <FormMessage type="error">{errorAPI}</FormMessage>}
-            <form
-                className="d-flex flex-column align-center needs-validation"
-                onSubmit={handleSubmit(handleLogin)}
-            >
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input
-                        id="email"
-                        type="email"
-                        className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-                        placeholder="johndoe@gmail.com"
-                        {...register("email", {
-                            required: "The email is required",
-                            pattern: {
-                                value: /\S+@\S+\.\S+/,
-                                message: "E-mail no válido",
-                            },
-                        })}
-                    />
-                    {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input
-                        id="password"
-                        type="password"
-                        className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                        {...register('password', {
-                            required: 'Password is required',
-                            minLength: {
-                                value: 8,
-                                message: 'Password must have at least 8 characters long'
-                            },
-                        })}
-                    />
-                    {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
-                </div>
 
-
-                <button className="btn btn-primary">{isLoading ? 'Signing in...' : 'Sign In'}</button>
-            </form>
-
-            <p className="text-center" style={{ marginTop: 20 }}>You don't have an account? <Link to={'/auth/register'} className="">Sign up</Link></p>
-
+        <div className="d-flex align-items-center vh-100">
+            <div className="container w-50 border border-light rounded p-4">
+                <h1 className="text-center">Task4</h1>
+                <hr />
+                <h2 className="mt-10">Sign In</h2>
+                {errorAPI && <FormMessage type="error">{errorAPI}</FormMessage>}
+                <form
+                    className="d-flex flex-column align-center needs-validation"
+                    onSubmit={handleSubmit(handleLogin)}
+                >
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input
+                            id="email"
+                            type="email"
+                            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                            placeholder="johndoe@gmail.com"
+                            {...register("email", {
+                                required: "The email is required",
+                                pattern: {
+                                    value: /\S+@\S+\.\S+/,
+                                    message: "E-mail no válido",
+                                },
+                            })}
+                        />
+                        {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input
+                            id="password"
+                            type="password"
+                            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                            {...register('password', {
+                                required: 'Password is required',
+                                minLength: {
+                                    value: 8,
+                                    message: 'Password must have at least 8 characters long'
+                                },
+                            })}
+                        />
+                        {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
+                    </div>
+                    <button className="btn btn-primary">{isLoading ? 'Signing in...' : 'Sign In'}</button>
+                </form>
+                <p className="text-center" style={{ marginTop: 20 }}>You don't have an account? <Link to={'/auth/register'} className="">Sign up</Link></p>
+            </div>
         </div>
     )
 }
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -51,79 +51,79 @@ const RegisterView = () => {
     }
 
     return (
-        <div className="container w-50">
-            <h1 className="text-center">Task4</h1>
-            <hr />
-            <h2 className="mt-10">Register account</h2>
-            {apiResponse?.message && <FormMessage type={apiResponse.type}>{apiResponse.message}</FormMessage>}
-            <form
-                className="d-flex flex-column align-center needs-validation"
-                onSubmit={handleSubmit(handleRegister)}
-            >
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input
-                        type="text"
-                        className={`form-control ${errors.name ? 'is-invalid' : ''}`}
-                        id="name"
-                        placeholder="John Doe"
-                        {...register('name', {
-                            required: 'The name is required',
-                        })}
-                    />
-                    {errors.name && <div className="invalid-feedback">{errors.name.message}</div>}
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input
-                        id="email"
-                        type="email"
-                        className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-                        placeholder="johndoe@gmail.com"
-                        {...register("email", {
-                            required: "The email is required",
-                            pattern: {
-                                value: /\S+@\S+\.\S+/,
-                                message: "E-mail no válido",
-                            },
-                        })}
-                    />
-                    {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input
-                        id="password"
-                        type="password"
-                        className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                        {...register('password', {
-                            required: 'Password is required',
-                            minLength: {
-                                value: 8,
-                                message: 'Password must have at least 8 characters long'
-                            },
-                        })}
-                    />
-                    {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password_confirmation" className="form-label">Confirm your password</label>
-                    <input
-                        id="password_confirmation"
-                        type="password"
-                        className={`form-control ${errors.password_confirmation ? 'is-invalid' : ''}`}
-                        {...register('password_confirmation', {
-                            required: 'Please confirm your password',
-                            validate: value => value === password || 'Passwords do not match',
-                        })}
-                    />
-                    {errors.password_confirmation && <div className="invalid-feedback">{errors.password_confirmation.message}</div>}
-                </div>
-
-                <button className="btn btn-primary">{isLoading ? ' Creating account...' : 'Create account'}</button>
-            </form>
-
-            <p className="text-center" style={{ marginTop: 20 }}>You have an account? <Link to={'/auth/login'} className="">Sign in!</Link></p>
+        <div className="d-flex align-items-center vh-100">
+            <div className="container w-50 border border-light rounded p-4">
+                <h1 className="text-center">Task4</h1>
+                <hr />
+                <h2 className="mt-10">Register account</h2>
+                {apiResponse?.message && <FormMessage type={apiResponse.type}>{apiResponse.message}</FormMessage>}
+                <form
+                    className="d-flex flex-column align-center needs-validation"
+                    onSubmit={handleSubmit(handleRegister)}
+                >
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input
+                            type="text"
+                            className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+                            id="name"
+                            placeholder="John Doe"
+                            {...register('name', {
+                                required: 'The name is required',
+                            })}
+                        />
+                        {errors.name && <div className="invalid-feedback">{errors.name.message}</div>}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input
+                            id="email"
+                            type="email"
+                            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                            placeholder="johndoe@gmail.com"
+                            {...register("email", {
+                                required: "The email is required",
+                                pattern: {
+                                    value: /\S+@\S+\.\S+/,
+                                    message: "E-mail no válido",
+                                },
+                            })}
+                        />
+                        {errors.email && <div className="invalid-feedback">{errors.email.message}</div>}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input
+                            id="password"
+                            type="password"
+                            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                            {...register('password', {
+                                required: 'Password is required',
+                                minLength: {
+                                    value: 8,
+                                    message: 'Password must have at least 8 characters long'
+                                },
+                            })}
+                        />
+                        {errors.password && <div className="invalid-feedback">{errors.password.message}</div>}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password_confirmation" className="form-label">Confirm your password</label>
+                        <input
+                            id="password_confirmation"
+                            type="password"
+                            className={`form-control ${errors.password_confirmation ? 'is-invalid' : ''}`}
+                            {...register('password_confirmation', {
+                                required: 'Please confirm your password',
+                                validate: value => value === password || 'Passwords do not match',
+                            })}
+                        />
+                        {errors.password_confirmation && <div className="invalid-feedback">{errors.password_confirmation.message}</div>}
+                    </div>
+                    <button className="btn btn-primary">{isLoading ? ' Creating account...' : 'Create account'}</button>
+                </form>
+                <p className="text-center" style={{ marginTop: 20 }}>You have an account? <Link to={'/auth/login'} className="">Sign in!</Link></p>
+            </div>
         </div>
     )
 }
